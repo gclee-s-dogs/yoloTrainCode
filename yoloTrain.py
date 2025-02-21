2a2b3b8aba9f17cca37a823fe06b7a4a83e03274
LeeKId-8945 <wiwi4910@gmail.com>
2025-02-21 23:53:55 +0800

    hi

---
+++ yoloTrain.py
@@ -0,0 +1,6 @@
+from ultralytics import YOLO
+
+if __name__ == '__main__':
+    model = YOLO("yolo11n.pt")
+    # Train the model
+    results = model.train(data="datasets/data.yaml", epochs=100, imgsz=640, batch=16, device='0')  # 改成 'cpu' 或 0
