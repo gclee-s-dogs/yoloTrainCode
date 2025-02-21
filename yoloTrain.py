0b22d58b01b01859677575a04e98bb058ac04bc3
LeeKId-8945 <wiwi4910@gmail.com>
2025-02-21 23:57:34 +0800

    Update yoloTrain.py

--- yoloTrain.py
+++ yoloTrain.py
@@ -4,3 +4,4 @@ if __name__ == '__main__':
     model = YOLO("yolo11n.pt")
     # Train the model
     results = model.train(data="datasets/data.yaml", epochs=100, imgsz=640, batch=16, device='0')  # 改成 'cpu' 或 0
+
